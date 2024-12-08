a4ef5b4a90966cf8257971a81a2b581de01c08dd
Praveen Kumar <praveen9166050@gmail.com>
2024-12-08 21:15:26 +0530

    Get earnings report controller

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,8 +1,35 @@
+const mongoose = require("mongoose");
+const Earning = require("../models/Earning");
+
 const getEarningsReport = async (req, res, next) => {
     try {
+        const {userId} = req.params;
+        const totalEarnings = await Earning.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    total: {$sum: '$profit'}
+                }
+            }
+        ]);
+        const levelWiseEarnings = await Earning.aggregate([
+            {
+                $match: {userId: new mongoose.Types.ObjectId(userId)}
+            },
+            {
+                $group: {
+                    _id: '$level',
+                    total: {$sum: '$profit'}
+                }
+            }
+        ]);
+        const earningsHistory = await Earning.find({userId}).sort({createdAt: -1});
         res.status(200).json({
             success: true,
-            message: "Earnings report fetched successfully"
+            message: "Earnings report fetched successfully",
+            totalEarnings: totalEarnings[0].total,
+            levelWiseEarnings,
+            earningsHistory
         });
     } catch (error) {
         next(error);
