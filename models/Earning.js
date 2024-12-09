b9f5ce43367df81b3cc41fc10c4db409fc1c13e4
Praveen Kumar <praveen9166050@gmail.com>
2024-12-09 23:17:37 +0530

    Created index in models

--- models/Earning.js
+++ models/Earning.js
@@ -4,7 +4,8 @@ const earningSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
+        required: true,
+        index: true
     },
     referredUserId: {
         type: mongoose.Schema.Types.ObjectId,
